ad02d207c6d66f358f42b4b56d1435c88046b0b3
Jason Ly <gamedude_7@hotmail.com>
2025-01-30 16:42:50 -0500

    AI pathfinding

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.AI;
 
 public class Person : MonoBehaviour
 {
+
+
+    public NavMeshAgent agent;
+
     public AudioClip otherClip;
     public AudioSource audioSource;
     public GameObject tornado;
     public float moveSpeed;
+
+    public List<GameObject> safePts;
+    private float distanceToTornado = 0;
+
     public void Scream()
     {
         audioSource.clip = otherClip;
@@ -27,42 +36,61 @@ public class Person : MonoBehaviour
     {
        
         if (!transform.GetComponent<Rigidbody>().freezeRotation)
-        { 
-            this.enabled = false;
+        {
+            agent.enabled = false;
+            this.enabled = false;            
             return;
         }
       
         if (tornado != null)
         {
-            
-            Vector3 dir = transform.position - tornado.transform.position;
-            
-            dir.Normalize();
-            Vector3 direction = dir;
-            //dir = dir / 100;
-            dir.y = 0;
-            RaycastHit raycastHit;
-            Debug.DrawRay(transform.position, direction  );
-            if (Physics.Raycast(transform.position, direction , out raycastHit, direction.magnitude)) 
-            {             
-               //if (raycastHit.collider.name != "Plane")
-               // { 
-               
-                    Debug.Log(this.name + "collided with " + raycastHit.collider.name);
-                Debug.DrawRay(transform.position + direction, raycastHit.normal);
-                //Debug.DrawRay(transform.position , direction + raycastHit.collider.transform.right);
-                 //Debug.Break();
-                // }               
-                dir = direction + raycastHit.normal;
-                dir.Normalize();
-               // dir = dir / 100;
-                dir.y = 0;
-            }                
-            this.gameObject.GetComponent<Rigidbody>().velocity = dir* moveSpeed;
-           // this.gameObject.transform.Translate(dir);
-            
 
-           
+            //Vector3 dir = transform.position - tornado.transform.position;
+
+            //dir.Normalize();
+            //Vector3 direction = dir;
+            ////dir = dir / 100;
+            //dir.y = 0;
+            //RaycastHit raycastHit;
+            //Debug.DrawRay(transform.position, direction  );
+            //if (Physics.Raycast(transform.position, direction , out raycastHit, direction.magnitude)) 
+            //{             
+            //   //if (raycastHit.collider.name != "Plane")
+            //   // { 
+
+            //        Debug.Log(this.name + "collided with " + raycastHit.collider.name);
+            //    Debug.DrawRay(transform.position + direction, raycastHit.normal);
+            //    //Debug.DrawRay(transform.position , direction + raycastHit.collider.transform.right);
+            //     //Debug.Break();
+            //    // }               
+            //    dir = direction + raycastHit.normal;
+            //    dir.Normalize();
+            //   // dir = dir / 100;
+            //    dir.y = 0;
+            //}                
+            //this.gameObject.GetComponent<Rigidbody>().velocity = dir* moveSpeed;
+            //// this.gameObject.transform.Translate(dir);
+            ///\
+            //
+            
+            distanceToTornado = (transform.position - tornado.transform.position ).magnitude;
+            float distance = distanceToTornado;
+            GameObject safestPt = safePts[0];
+            foreach (GameObject safePt in safePts)
+            {
+                if (safePt != null)
+                {
+                    Vector3 distTornadoToSafePt = safePt.transform.position - tornado.transform.position;
+                    Vector3 distToTornado = tornado.transform.position - transform.position;
+
+                    if (distTornadoToSafePt.magnitude > distance && Vector3.Dot(distTornadoToSafePt,distToTornado) < 0)
+                    {
+                        safestPt = safePt;
+                        distance = distTornadoToSafePt.magnitude;
+                    }
+                }
+            }
+            agent.SetDestination(safestPt.transform.position);
         }
     }
 }
